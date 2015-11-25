f1560ae0e5e3c5baa49d86ed0c28059cfbf3a878
Mad Bernard <madeline.a.bernard@gmail.com>
2015-11-25 10:23:50 -0800

    First commit, some specs copied in

---
+++ union-builder.js
@@ -0,0 +1,14 @@
+//us flag star mapper -- for a given number of stars, how do they condense into a square
+//height of "union" is 7/13, width of union is 0.76
+
+//make set of arrays where two numbers, height and width, are
+//reduce arrays to find array with minimum deviation (%) from 7/13 / 0.76
+//return that array
+
+//array sets can be:  even and odd rows where odd can be 1 more than even
+//or where both even and odd are equal
+//or where this test is done with half the total and then doubled (symmetry across horizontal center)
+//or where first line or lines follow different pattern from middle lines
+
+//https://madbernard.dreamwidth.org/2110.html
+//http://www.usflag.org/flag.specs.html
