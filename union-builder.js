d0c3ee34245608cc9d0897272634e530b476b2a9
Mad Bernard <madeline.a.bernard@gmail.com>
2015-11-25 11:15:16 -0800

    More planning

--- union-builder.js
+++ union-builder.js
@@ -19,7 +19,24 @@
 // store the best fits in an array that I look up before re-running that query...  memoize?  Object?
 // yes, store in object with key as the input number of states and value as the best pattern
 
-// function that is for even numbers of rows
+// // function that is for even numbers of rows
+//   start at num
+  // for loop to get i''s
+  // loop ends when num / i < i
+  //   push columns: math.floor (num/i), rows: i
+  //   push columns: i, rows: math.floor (num/i)
+
+// above should get us all the multiples that come in under num?
+ maybe better to just get all numbers that multiplied together come in under num
+ recursion
+
+ then take that list of multiples and get the total stars in them
+ no, have recursion also provide total as key, row and column as arrays in key
+ then for each total see if there is another total that combined add up to num
+ and see if those fit the bill
+ ie see that the wider set of rows is an even #
+
+
 // function that is for any numbers of rows
 // function that adds above functions
 
