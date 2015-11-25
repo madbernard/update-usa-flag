6ce1605f3896792437b5986ea8078d6a422b91d6
Mad Bernard <madeline.a.bernard@gmail.com>
2015-11-25 12:03:02 -0800

    adds testing suite https://github.com/phillipalexander/generator-test

---
+++ spec/union-builder.js
@@ -0,0 +1,20 @@
+/* global union-builder, describe, it, expect, should */
+
+describe('union-builder()', function () {
+  'use strict';
+
+  it('exists', function () {
+    expect(union-builder).to.be.a('function');
+
+  });
+
+  it('does something', function () {
+    expect(true).to.equal(false);
+  });
+
+  it('does something else', function () {
+    expect(true).to.equal(false);
+  });
+
+  // Add more assertions here
+});
